e7e2ae7f6237f8e3adf006f89b029572839cfec1
Asma Jama <asmajama98@gmail.com>
2023-03-19 22:01:41 -0400

    Added timer function

---
+++ timer1.js
@@ -0,0 +1,11 @@
+const args = process.argv.slice(2);
+
+for (let i = 0; i < args.length; i++) {
+  const num = Number(args[i]);
+  if (!isNaN(num) && num >= 0) {
+    setTimeout(() => {
+      console.log(`Timer ${i+1} beeped after ${num} seconds.`);
+      process.stdout.write('\x07');
+    }, num * 1000);
+  }
+}
