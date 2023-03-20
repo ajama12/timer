dd90c346b092ca636c85e65dffe83e6d3a1fd809
Asma Jama <asmajama98@gmail.com>
2023-03-19 22:02:48 -0400

    Fixed typo

--- timer1.js
+++ timer1.js
@@ -4,7 +4,7 @@ for (let i = 0; i < args.length; i++) {
   const num = Number(args[i]);
   if (!isNaN(num) && num >= 0) {
     setTimeout(() => {
-      console.log(`Timer ${i+1} beeped after ${num} seconds.`);
+      console.log(`Timer ${i + 1} beeped after ${num} seconds.`);
       process.stdout.write('\x07');
     }, num * 1000);
   }
